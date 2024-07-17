f5d15fd7e4e74745debb502201526221f7adfcfd
AndrRok <rokhmanov2000@yandex.ru>
2024-07-17 23:04:53 +0400

    Initial Commit

---
+++ TestCustomNavigation/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TestCustomNavigation
+//
+//  Created by ARMBP on 7/17/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TestCustomNavigation/TestCustomNavigationApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestCustomNavigationApp.swift
+//  TestCustomNavigation
+//
+//  Created by ARMBP on 7/17/24.
+//
+
+import SwiftUI
+
+@main
+struct TestCustomNavigationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
